b850d7b22ef89215cc0e803288e7eee5878e8c6d
AjvDani <josedanielvilla06@gmail.com>
2025-05-31 12:29:51 -0500

    Add files via upload

---
+++ Codigo_Proyecto1_Grupo 5/Pilas/Pila.hpp
@@ -0,0 +1,70 @@
+#ifndef PILA_HPP
+#define PILA_HPP
+
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class Pila {
+private:
+    struct Nodo {
+        T dato;
+        Nodo* siguiente;
+    };
+
+    Nodo* cima;
+
+public:
+    Pila() : cima(nullptr) {}
+
+    void insertarPagina(const string& dato) {
+        Nodo* nuevo = new Nodo{dato, cima};
+        cima = nuevo;
+    }
+
+    void retirarPagina() {
+        if (cima != nullptr) {
+            Nodo* temp = cima;
+            cima = cima->siguiente;
+            delete temp;
+        } else {
+            cout << "Stack Underflow (no hay más páginas)" << endl;
+        }
+    }
+
+    string verPaginaActual() const {
+        return (cima != nullptr) ? cima->dato : "Sin página";
+    }
+
+    bool estaVacia() {
+        return cima == nullptr;
+    }
+
+    void mostrarHistorial() const {
+        Nodo* actual = cima;
+        while (actual) {
+            cout << "\n--------------\n";
+            cout << actual->dato;
+            actual = actual->siguiente;
+        }
+    }
+
+    int contarPaginas() {
+        int contador = 0;
+        Nodo* actual = cima;
+        while (actual) {
+            actual = actual->siguiente;
+            contador++;
+        }
+        return contador;
+    }
+
+    ~Pila() {
+        while (!estaVacia()) {
+            retirarPagina();
+        }
+    }
+};
+
+#endif // PILA_HPP
